7b2156b499bd5c44aa50ad8ea802684157454d62
josuaobando <josua.obando@gmail.com>
2017-08-05 17:07:28 -0600

    Delete file

--- src/app.config.ts
+++
@@ -1,7 +0,0 @@
-export class AppSettings{
-
-    public static WS_API = 'http://api.dinerosegurohf:8080/api/controller.php';
-    public static WS_AUTH = 'testkey';
-    public static isDEV = true;
-
-}
